cda47c7b8c312d907e50aafa1a8591cdc0ce886a
Djapedjape123 <petafipedjaradic@gmail.com>
2026-08-15 16:44:10 +0200

    pocetak registracije

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,6 +6,6 @@ const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 if (!supabaseUrl || !supabaseServiceKey) {
   throw new Error('Nedostaju Supabase varijable u .env.local fajlu!');
 }
-
+//supabase
 // Admin klijent koji ima pune privilegije na backendu za upis vektora
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
